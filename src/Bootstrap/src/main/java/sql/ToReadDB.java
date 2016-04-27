bfa968e9fe5842a97a1f48e8089b465b4da8b94a
kazb15@student.bth.se <kazb15@student.bth.se>
2016-04-27 20:07:59 +0200

    files to read info from database added to bootstrap

---
+++ src/Bootstrap/src/main/java/sql/ToReadDB.java
@@ -0,0 +1,14 @@
+package sql;
+//package com.javadbreader.octopus;
+//
+///**
+// * @author Kambiz
+// */
+//
+//      public class ToReadDB {
+//    	  public static void main(String args[]){
+//    		  
+//    		  DBReadWrite object = new DBReadWrite();
+//	    	 
+//	     }
+//}
