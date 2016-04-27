020db628c4f080c8a473b9d310a4a1b7bd0b9b06
Kamran <kamran.alipour@gmail.com>
2016-04-27 12:53:33 +0200

    GUI initial files added to the project

---
+++ src/client/src/gui/Main.java
@@ -0,0 +1,18 @@
+//package gui;
+//import java.awt.EventQueue;
+//
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					Client window = new Client();
+//					window.frmOctopusP2P.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
+//}
