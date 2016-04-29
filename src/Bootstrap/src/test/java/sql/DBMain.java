6d49242a58c99ad116b6f64b7d88a7b090e06f79
kazb15@student.bth.se <kazb15@student.bth.se>
2016-04-29 11:14:14 +0200

    added databaseReaderWriter files and the usage doc

---
+++ src/Bootstrap/src/test/java/sql/DBMain.java
@@ -0,0 +1,13 @@
+package com.javadbwriter.octopus;
+
+/**
+ * @author Kambiz
+ */
+
+      public class DBMain {
+    	  public static void main(String args[]){
+    		  
+    		  DBReadWriteClass object = new DBReadWriteClass();
+	    	 
+	     }
+}
---
+++ src/Bootstrap/src/test/java/sql/DBReadWriteClass.java
@@ -0,0 +1,55 @@
+package com.javadbwriter.octopus;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * @author Kambiz
+ */
+
+public class DBReadWriteClass {
+	
+		Connection connect = null;
+		Statement stmnt = null;
+		ResultSet rs = null;
+		
+	public DBReadWriteClass(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			String connectionUrl = "jdbc:mysql://localhost:3306/serverdb?autoReconnect=true&useSSL=false";
+			String connectionUser = "root";
+			String connectionPassword = "";
+			connect = DriverManager.getConnection(connectionUrl, connectionUser, connectionPassword);
+			System.out.println("Connected to server successfully...\n");
+			stmnt = connect.createStatement();
+			
+			stmnt.executeUpdate("INSERT INTO serverpeers (id, latestIP, blacklist, timestamp, files, filecount) " + 
+	                "VALUES (1026, '192.168.31.52', 1, default,'Captain Ameerica Civil War', 0)");
+			rs = stmnt.executeQuery("SELECT * FROM serverpeers");
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String latestIP = rs.getString("latestIP");
+				String blackList = rs.getString("blacklist");
+				String timestamp = rs.getString("timestamp");
+				String files = rs.getString("files");
+				String filecount = rs.getString("filecount");
+				System.out.println("ID: " + id + 
+						"\nLastestIP: " + latestIP + 
+						"\nBlacklist: " + blackList +
+						"\nTimestamp: " + timestamp +
+						"\nFiles: " + files +
+						"\nFilecount: " + filecount
+						);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+//			try { if (rs != null) rs.close(); } catch (SQLException e) { e.printStackTrace(); }
+			try { if (stmnt != null) stmnt.close(); } catch (SQLException e) { e.printStackTrace(); }
+			try { if (connect != null) connect.close(); } catch (SQLException e) { e.printStackTrace(); }
+		}
+	}
+}
