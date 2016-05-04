1ca3c95e3e536d322358e4be6e176de871e6bcdf
mhan10 <theiimaxii@gmail.com>
2016-05-04 10:41:26 +0200

    Added bootstrap graceful shutdown

---
+++ src/Bootstrap/src/main/java/rest/Main.java
@@ -0,0 +1,22 @@
+package rest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		final Thread restServerThread = new Thread(new RESTStartUp());
+		restServerThread.start();
+
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				restServerThread.interrupt();
+				try {
+					restServerThread.join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
--- src/Bootstrap/src/main/java/rest/RESTStartUp.java
+++ src/Bootstrap/src/main/java/rest/RESTStartUp.java
@@ -4,19 +4,31 @@ import java.io.IOException;
 import com.sun.jersey.api.container.httpserver.HttpServerFactory;
 import com.sun.net.httpserver.HttpServer;
 
-public class RESTStartUp {
-    static final String BASE_URI = "http://localhost:9999/rest/";
-    public static void main(String[] args) {
-        try {
-            HttpServer server = HttpServerFactory.create(BASE_URI);
-            server.start();
-            System.out.println("Press Enter to stop the server. ");
-            System.in.read();
-            server.stop(0);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+public class RESTStartUp implements Runnable {
+	static final String BASE_URI = "http://localhost:9999/rest/";
+
+	public void run() {
+		HttpServer server = null;
+		try {
+			server = HttpServerFactory.create(BASE_URI);
+			server.start();
+			System.out.println("Bootstrap server was started at " + server.getAddress().toString());
+			while (true) {
+				Thread.sleep(1000);
+			}
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			System.out.println("Bootstrap server was stopped");
+			server.stop(0);
+			server = null;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (server != null) {
+				System.out.println("Bootstrap server was stopped (force)");
+				server.stop(0);
+			}
+		}
+	}
 }
