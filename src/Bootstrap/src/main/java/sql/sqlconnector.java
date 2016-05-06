5887ab27330d3436d0e1318c42f13a5bbe115f19
Ahmad, Farhan <farhanahmad243@yahoo.com>
2016-05-06 06:08:09 +0200

    Updated SQL Class to Auto Create Bootstrap DB

--- src/Bootstrap/src/main/java/sql/sqlconnector.java
+++ src/Bootstrap/src/main/java/sql/sqlconnector.java
@@ -132,6 +132,98 @@ public boolean Update (String update) {
     return true;
 }
 
+//Function to Create Server DB Once
+public void createserverdb(){
+	//Create Table01 servers
+	try {
+		DatabaseMetaData dbm = connection.getMetaData();
+		ResultSet tables = dbm.getTables(null, null, "servers", null);
+		if (tables.next()) {
+			  // Table exists Don't Create Table
+			}
+			else {
+				//Table Doesn't Exist, Create Table
+				String createtable = " CREATE TABLE servers ( " +
+					    	" ip varchar(100) NOT NULL, " +
+					    	" name char(20) NOT NULL, " +
+					    	" timestamp timestamp NOT NULL, " +
+					    	" clientcount int NOT NULL, " +
+					    	" servercount int NOT NULL, " +
+					    	" CONSTRAINT Servers_pk PRIMARY KEY (ip))";
+
+				this.Update(createtable);
+			}
+  }
+  catch (Exception e) {
+      System.out.println("Exception in query method:\n" + e.getMessage());
+  }
+	//Create Table02 peersarray
+	try {
+		DatabaseMetaData dbm = connection.getMetaData();
+		ResultSet tables = dbm.getTables(null, null, "peersarray", null);
+		if (tables.next()) {
+			  // Table exists Don't Create Table
+			}
+			else {
+				//Table Doesn't Exist, Create Table
+				//Table Doesn't Exist, Create Table
+				String createtable = "CREATE TABLE peersarray ( " +
+							" uniquefileid varchar(100) NOT NULL, " +    
+							" peers varchar(100) NOT NULL, " +
+							" CONSTRAINT peersarray_pk PRIMARY KEY (uniquefileid))";
+				this.Update(createtable);
+			}
+  }
+  catch (Exception e) {
+      System.out.println("Exception in query method:\n" + e.getMessage());
+  }
+	//Create Table03 serverfile
+		try {
+			DatabaseMetaData dbm = connection.getMetaData();
+			ResultSet tables = dbm.getTables(null, null, "serverfile", null);
+			if (tables.next()) {
+				  // Table exists Don't Create Table
+				}
+				else {
+					//Table Doesn't Exist, Create Table
+					String createtable = "CREATE TABLE serverfile ( " +
+					    	" filename char(40) NOT NULL, " +
+					    	" totalblocks int NOT NULL, " +
+					    	" peers varchar(100) NOT NULL, " +
+					    	" peercount int NOT NULL, " +
+					    	" uniquefileid varchar(100) NOT NULL, " +
+					    	" filechecksum varchar(100) NOT NULL, " +
+					    	" metadatachecksum varchar(100) NOT NULL, " +		
+					    	" CONSTRAINT serverfile_pk PRIMARY KEY (uniquefileid))";
+					this.Update(createtable);
+				}
+	    }
+	    catch (Exception e) {
+	        System.out.println("Exception in query method:\n" + e.getMessage());
+	    }
+	//Create Table04 serverpeers
+	try {
+		DatabaseMetaData dbm = connection.getMetaData();
+		ResultSet tables = dbm.getTables(null, null, "serverpeers", null);
+		if (tables.next()) {
+			  // Table exists Don't Create Table
+			}
+			else {
+				//Table Doesn't Exist, Create Table
+				String createtable = "CREATE TABLE serverpeers ( " +
+							" id int NOT NULL, " +
+						    " latestIP varchar(100) NOT NULL, " +
+						    " blacklist binary(1) NOT NULL, " +
+						    " timestamp timestamp NOT NULL, " +
+						    " CONSTRAINT serverpeers_pk PRIMARY KEY (id))";
+				this.Update(createtable);
+			}
+  }
+  catch (Exception e) {
+      System.out.println("Exception in query method:\n" + e.getMessage());
+  }
+}
+
 /*public boolean write (String write) {
 
     try {
