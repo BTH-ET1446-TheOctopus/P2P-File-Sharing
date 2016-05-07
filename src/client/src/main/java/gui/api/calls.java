2a1c3b9b766b151875c1881bd37d23128fe5984c
Kamran Alipoursimakani <kamran.alipour@gmail.com>
2016-05-07 13:34:59 +0200

    Cleared everything in Client.java in order to break it down to smaller commits

---
+++ src/client/src/main/java/gui/api/calls.java
@@ -0,0 +1,9 @@
+package gui.api;
+
+public interface calls
+{
+	
+	public boolean getStatus();
+	public void setTable(String name, String percent, String size, String speed, String finishTime, String addedTime);
+
+}
