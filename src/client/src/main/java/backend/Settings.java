f86e2e084f2c78d105bb14203619bd1465c1c3e2
Berntsson, Fredrik <freber@outlook.com>
2016-05-07 12:10:09 +0200

    Fix client rest server

--- src/client/src/main/java/backend/Settings.java
+++ src/client/src/main/java/backend/Settings.java
@@ -4,6 +4,6 @@ public final class Settings
 {
 	public static String clientPortNr = "1337";
 	public static String bootstrapURL = "http://localhost:9999/rest/rest/";
-	public static String clientURL = "https://0.0.0.0:1337";
+	public static String clientURL = "http://0.0.0.0:1337/";
 	
 }
