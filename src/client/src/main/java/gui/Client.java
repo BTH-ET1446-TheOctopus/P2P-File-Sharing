954b6aded7d1e152453dffb55a3e17b6f6d2278b
Kamran Alipoursimakani <kamran.alipour@gmail.com>
2016-05-07 13:59:03 +0200

    Created the main frame of the client application

--- src/client/src/main/java/gui/Client.java
+++ src/client/src/main/java/gui/Client.java
@@ -1,4 +1,24 @@
-public class Client
+package gui;
+
+import javax.swing.JFrame;
+
+public class Client extends JFrame
 {
-	
+
+	//Defining a public variable with JFrame type
+	public JFrame frame;
+
+	public Client()
+	{
+
+		frame = new JFrame("The Octopus P2P");
+		frame.setSize(680, 400);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+		
+	}
+
 }
