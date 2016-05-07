c892f0e49bd97255dd3ec5b0671df68a029ecf3b
Kamran Alipoursimakani <kamran.alipour@gmail.com>
2016-05-07 18:41:29 +0200

    Added opening file functionality to the newTorrent button

--- src/client/src/main/java/gui/Client.java
+++ src/client/src/main/java/gui/Client.java
@@ -2,7 +2,12 @@ package gui;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -10,12 +15,6 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.ImageIcon;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.awt.event.ActionEvent;
 
 public class Client
 {
