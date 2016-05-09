7828d0c20987d8aa4c9900e0f2591340719c12ff
Kukkapalli,NagaVyshnavi <vyshnavi994@gmail.com>
2016-05-09 12:29:44 +0200

    Comments added to Bootstarp main

--- src/Bootstrap/src/main/java/backend/Main.java
+++ src/Bootstrap/src/main/java/backend/Main.java
@@ -1,18 +1,24 @@
 package backend;
 
 import backend.rest.RESTStartUp;
-/**
+
+/*
  * starts the rest server in a new thread and adds a shutdown hook 
  * that is excuted when the program exits
  * 
  */
 
 public class Main {
-	/**
+/*
 	 * #@param args
 	 */
 
 	public static void main(String[] args) {
+		/*The below command line is calling a method/class "RESTStartUp"
+		 where the bootstrap is initiated, interrupted and terminated  */
+		
+		
+		
 		final Thread restServerThread = new Thread(new RESTStartUp());
 		restServerThread.start();
 
@@ -20,10 +26,12 @@ public class Main {
 			@Override
 			public void run() {
 				restServerThread.interrupt();
+//This command line is allowing the user to join the peer network again 
+//if the connection is interrupted
 				try {
-					restServerThread.join();
+					restServerThread.join();//This command line is defining the user to join the peer network
 				} catch (InterruptedException e) {
-					e.printStackTrace();
+					e.printStackTrace();//Jakson is disconnected from peer network
 				}
 			}
 		});
