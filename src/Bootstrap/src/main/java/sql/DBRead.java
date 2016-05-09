a82eb0be268a2c40cf58b6a0681ded6eca4f6974
kazb15@student.bth.se <kazb15@student.bth.se>
2016-05-09 22:21:39 +0200

    getPeerArray() method added for serverdb

--- src/Bootstrap/src/main/java/sql/DBRead.java
+++ src/Bootstrap/src/main/java/sql/DBRead.java
@@ -111,5 +111,29 @@ public class DBRead {
 		}
 		
 	} 
+	
+	public void getPeerArray() {  //This method reads from 'peersarray' table
+		rs = sc.runquery("SELECT * FROM peersarray");
+
+		try {
+			while(rs.next()){
+				//Retrieve by column name
+				String uniquefileid=rs.getString("uniquefileid");
+				String peers=rs.getString("peers");
+
+				//Display values
+				LOG.log(Level.INFO, "\nuniquefileid: " + uniquefileid.toString() + 
+						"\npeers: " + peers.toString());
+			}
+		} catch (SQLException ex){
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		} finally  {
+			sc.closeconnect();
+		}	
+
+	}
 
 }
