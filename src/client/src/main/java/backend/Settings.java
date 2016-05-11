66edf7ac0ce07245199b645717c45a907d2fbe80
mhan10 <theiimaxii@gmail.com>
2016-05-11 16:45:37 +0200

    Added keyword final to variables in backend.Settings

--- src/client/src/main/java/backend/Settings.java
+++ src/client/src/main/java/backend/Settings.java
@@ -2,9 +2,9 @@ package backend;
 
 public final class Settings 
 {
-	public static String clientPortNr = "1337";
-	public static String bootstrapURL = "http://localhost:9999/rest/";
-	public static String clientURL = "http://0.0.0.0:1337/";
+	public static final String CLIENT_PORT = "1337";
+	public static final String BOOTSTRAP_URL = "http://localhost:9999/rest/";
+	public static final String CLIENT_URL = "http://localhost:1337/";
 	
 	public static final int HELLO_INTERVAL = 2 * 60; // seconds
 }
