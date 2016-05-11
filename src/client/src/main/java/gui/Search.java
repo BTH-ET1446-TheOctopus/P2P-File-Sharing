958688be28b791636ee1255867fe12ac3ac59e4e
Kamran Alipoursimakani <kamran.alipour@gmail.com>
2016-05-11 13:49:56 +0200

    Added HintTextField class to the Search.class in order to show hints to user when a textfield is not selected

--- src/client/src/main/java/gui/Search.java
+++ src/client/src/main/java/gui/Search.java
@@ -9,33 +9,35 @@ import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 
 public class Search extends JDialog
 {
 
-	private static final long serialVersionUID = 1L;
-	
-	private JDialog frame;
-	private JTextField searchTextField;
-	private JButton searchButton;
-	private JTable searchResultTable;
-	private JButton downloadButton;
-	private JTextField textField;
-	
+	private static final long	serialVersionUID	= 1L;
+
+	private JDialog				frame;
+	private HintTextField		searchTextField;
+	private JButton				searchButton;
+	private JTable				searchResultTable;
+	private JScrollPane			resultTableScrolPane;
+	private JButton				downloadButton;
+
 	public Search(JFrame parent)
 	{
 		createSearchJDialog(parent);
 		addSearchTextField();
-		addSearchButton();
-		addsearch2TextField();
-		addsearch2Button();
+		addServerSearchButton();
+		addLocalSearchButton();
 		addSearchResultTable();
 		addDownloadButton();
 	}
-	
+
 	/**
-	 * This method creates the Search Dialog and sets the Main frame of the Octopus P2P client as the parent for it
+	 * This method creates the Search Dialog and sets the Main frame of the
+	 * Octopus P2P client as the parent for it
+	 * 
 	 * @author Kamran Alipoursimakani
 	 */
 
@@ -50,114 +52,130 @@ public class Search extends JDialog
 		frame.setDefaultCloseOperation(HIDE_ON_CLOSE);
 		frame.setVisible(true);
 	}
-	
+
 	/**
 	 * This method adds "searchTextField" text field to the search dialog1
+	 * 
 	 * @author Kamran Alipoursimakani
 	 */
 
 	private void addSearchTextField()
 	{
-		searchTextField = new JTextField("Type here");
-		//searchTextField.setToolTipText("Enter what ever you are looking for here");
-		searchTextField.setBounds(6, 6, 390, 30);
+		searchTextField = new HintTextField("What is it that you are looking for.!?");
+		searchTextField.setBounds(6, 6, 588, 30);
+
 		frame.getContentPane().add(searchTextField);
-		
-		/**
-		 * This method handles the prompt text in searchTextField 
-		 */
-		
-		searchTextField.addFocusListener(new FocusListener(){
-			
-		    public void focusGained(FocusEvent e) {
-		    	searchTextField.setText("");
-		    }
-			public void focusLost(FocusEvent e) {
-				if(searchTextField.getText().isEmpty())
-				searchTextField.setText("Type here");
-			}
-			});
+
 	}
-	
+
 	/**
 	 * This method adds the "Search Neighbors" button to the search dialog
+	 * 
 	 * @author Kamran Alipoursimakani
 	 */
-	
-	private void addSearchButton()
+
+	private void addLocalSearchButton()
 	{
-		searchButton = new JButton("Search Neighbors");
-		searchButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		searchButton = new JButton("Search in Neighbors");
+		searchButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
 			}
 		});
-		searchButton.setBounds(408, 5, 180, 30);
+		searchButton.setBounds(414, 48, 180, 30);
 		frame.getContentPane().add(searchButton);
 	}
-	
-	/**
-	 * This method adds the "Search Neighbors" button to the search dialog2
-	 */
-	
-	private void addsearch2TextField()
-	{
-		textField = new JTextField("Type here");
-		//textField.setToolTipText("Enter what ever you are looking for here");
-		textField.setBounds(6, 48, 390, 30);
-		frame.getContentPane().add(textField);
-		
-		/**
-		 * This method displays prompt text in search2TextField 
-		 */
-		
-		textField.addFocusListener(new FocusListener(){
-			
-		    public void focusGained(FocusEvent e) {
-		    	textField.setText("");
-		    }
-			public void focusLost(FocusEvent e) {
-				if(textField.getText().isEmpty())
-				textField.setText("Type here");
-			}
-			});
-	}
-	
+
 	/**
 	 * This method adds the "Search on Servers" button to the search dialog2
-	 */ 
-	
-	private void addsearch2Button()
+	 */
+
+	private void addServerSearchButton()
 	{
 		searchButton = new JButton("Search on Server");
-		searchButton.setBounds(410, 48, 178, 30);
+		searchButton.setBounds(6, 48, 180, 30);
 		frame.getContentPane().add(searchButton);
 	}
-	
+
 	/**
 	 * This method adds the "searchResultTable" table to the search dialog
+	 * 
 	 * @author Kamran Alipoursimakani
 	 */
-	
+
 	private void addSearchResultTable()
 	{
 		searchResultTable = new JTable();
-		JScrollPane resultTableScrolPane = new JScrollPane(searchResultTable);
+		resultTableScrolPane = new JScrollPane(searchResultTable);
 		resultTableScrolPane.setBounds(6, 90, 588, 202);
-		resultTableScrolPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		resultTableScrolPane.setVerticalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		frame.getContentPane().add(resultTableScrolPane);
 	}
-	
+
 	/**
 	 * This method adds the "downloadButton" button to the search dialog
+	 * 
 	 * @author Kamran Alipoursimakani
 	 */
-	
+
 	private void addDownloadButton()
 	{
 		downloadButton = new JButton("Download");
 		downloadButton.setBounds(481, 310, 107, 30);
-		frame.getContentPane().add(downloadButton);		
-		
+		frame.getContentPane().add(downloadButton);
+
+	}
+}
+
+/**
+ * This class creates a TextField with grayed out hint text when not selected
+ * 
+ * @author Kamran Alipoursimakani
+ *
+ */
+
+class HintTextField extends JTextField implements FocusListener
+{
+
+	private static final long	serialVersionUID	= 1L;
+	private final String		hint;
+	private boolean				showingHint;
+
+	public HintTextField(final String hint)
+	{
+		super(hint);
+		this.hint = hint;
+		this.showingHint = true;
+		super.addFocusListener(this);
+	}
+
+	@Override
+	public void focusGained(FocusEvent e)
+	{
+		if (this.getText().isEmpty())
+		{
+			super.setText("");
+			showingHint = false;
+			super.setForeground(new Color(0, 0, 0));
+
+		}
+	}
+
+	@Override
+	public void focusLost(FocusEvent e)
+	{
+		if (this.getText().isEmpty())
+		{
+			super.setText(hint);
+			showingHint = true;
+			super.setForeground(new Color(210, 210, 210));
+
+		}
+	}
+
+	@Override
+	public String getText()
+	{
+		return showingHint ? "" : super.getText();
 	}
 }
