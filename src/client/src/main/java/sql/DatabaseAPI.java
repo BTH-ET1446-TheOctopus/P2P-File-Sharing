511bd68d6f55cf4444f22aab91e38229efba6531
Nyhlen, Jesper <nyhlen_91@hotmail.com>
2016-05-12 17:04:23 +0200

    Combined DBWrite and DBRead into DatabaseCalls, made interface DatabaseAPI (Client)

---
+++ src/client/src/main/java/sql/DatabaseAPI.java
@@ -0,0 +1,10 @@
+package sql;
+
+public interface DatabaseAPI {
+	public void getSwarm();
+	public void getPeers();
+	public void getPeerArray();
+	public void addPeerArray();
+	public void addSwarm();
+	public void addPeers();
+}
--- src/client/src/main/java/sql/DatabaseCalls.java
+++ src/client/src/main/java/sql/DatabaseCalls.java
@@ -1,24 +1,19 @@
 package sql;
 
-/* class to read data from each table */
-
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
-public class DBRead {
-
-	private static final Logger LOG = Logger.getLogger(DBRead.class.getName());
+public class DatabaseCalls implements DatabaseAPI{
+	private static final Logger LOG = Logger.getLogger(DatabaseCalls.class.getName());
 	
 	//create an object from sqlconnector, to be able to connect to the database
 	sqlconnector sc = new sqlconnector();
 
 	ResultSet rs = null;
 
-	public DBRead() {
-
-	}
 	
 	public void getSwarm() {  //This method reads from 'clientfile' table
 		rs = sc.runquery("SELECT * FROM clientfile where peercount='1'");
@@ -109,5 +104,51 @@ public class DBRead {
 		}	
 
 	} 
+	public void addPeerArray(){  //This method writes to 'peersarray' table
+		sqlconnector sc = new sqlconnector();
 
+		Statement stmnt = sc.getStatement();
+		
+		try {
+			stmnt.executeUpdate("INSERT INTO peersarray (uniquefileid, peers) " + 
+					"VALUES ( '2sdfd5sdf6dsf5', '192.168.54.68')");
+		} catch (SQLException e) {
+			LOG.log(Level.INFO, e.getMessage(), e);
+		}
+		finally {  //close all connection to database
+			sc.closeconnect();
+		}
+	}
+
+	public void addSwarm(){   //This method writes to 'clientfile' table
+		sqlconnector sc = new sqlconnector();
+
+		Statement stmnt = sc.getStatement();
+				
+		try {
+			stmnt.executeUpdate("INSERT INTO clientfile (filename, totalblocks, peers, peercount, uniquefileid, filechecksum, metadatachecksum) " + 
+					"VALUES ('Pirates Carrabian', 10000, '192.168.2.2', 1, 'sdfsdggh22255', 'filechecksum', 'metadatachecksum' )");
+		} catch (SQLException e) {
+			LOG.log(Level.INFO, e.getMessage(), e);
+		}
+		finally {  //close all connection to database
+			sc.closeconnect();
+		}
+	}
+	
+	public void addPeers(){  //This method writes to 'clientpeers' table
+		sqlconnector sc = new sqlconnector();
+
+		Statement stmnt = sc.getStatement();
+		
+		try {
+			stmnt.executeUpdate("INSERT INTO clientpeers (id, latestIP, blacklist, timestamp, files, filecount) " + 
+					"VALUES ('21dfg1df1g21g', '192.168.31.52', 0, default,'Captain Ameerica Civil War', 0)");
+		} catch (SQLException e) {
+			LOG.log(Level.INFO, e.getMessage(), e);
+		}
+		finally {  //close all connection to database
+			sc.closeconnect();
+		}
+	}
 }
