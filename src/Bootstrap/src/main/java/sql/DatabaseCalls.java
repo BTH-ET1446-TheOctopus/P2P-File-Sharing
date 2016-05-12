47f1599a231e31ab25b51cc28bceb9e3b8d051e0
kazb15@student.bth.se <kazb15@student.bth.se>
2016-05-12 22:09:58 +0200

    Modify some codes

--- src/Bootstrap/src/main/java/sql/DatabaseCalls.java
+++ src/Bootstrap/src/main/java/sql/DatabaseCalls.java
@@ -14,6 +14,69 @@ private static final Logger LOG = Logger.getLogger(DatabaseCalls.class.getName()
 	sqlconnector sc = new sqlconnector();
 
 	ResultSet rs = null;
+	
+	public void addBootstrapServer(String ip, String name, int clientcount, int servercount){  //This method writes to 'servers' table
+		sqlconnector sc = new sqlconnector();
+		Statement stmnt = sc.getStatement();
+		
+		try {
+			stmnt.executeUpdate("INSERT INTO servers (ip, name, timestamp, clientcount, servercount) " + 
+					"VALUES ('"+ip+"', '"+name+"', default,"+clientcount+", "+servercount+")");
+			//"VALUES ('192.168.54.68', 'Backup01', default,2, 1)");
+			
+		} catch (SQLException e) {
+			LOG.log(Level.INFO, e.getMessage(), e);
+		}
+		finally {  //close all connection to database
+			sc.closeconnect();
+		}
+	}
+
+	public void addSwarm(String filename, int totalblocks, String peers, int peercount, int uniquefileid){   //This method writes to 'serverfile' table
+		sqlconnector sc = new sqlconnector();
+		Statement stmnt = sc.getStatement();
+				
+		try {
+			stmnt.executeUpdate("INSERT INTO serverfile (filename, totalblocks, peers, peercount, uniquefileid, filechecksum, metadatachecksum) " + 
+					"VALUES ('"+filename+"', "+totalblocks+", '"+peers+"', "+peercount+", "+uniquefileid+",'filechecksum', 'metadatachecksum')");
+			//"VALUES ('Pirates Carrabian', 10000, '192.168.2.2', 1, 2255, 'filechecksum', 'metadatachecksum' )");
+		} catch (SQLException e) {
+			LOG.log(Level.INFO, e.getMessage(), e);
+		}
+		finally {  //close all connection to database
+			sc.closeconnect();
+		}
+	}
+	
+	public void addPeers(String id, String latestIP, int blacklist){  //This method writes to 'serverpeers' table
+		sqlconnector sc = new sqlconnector();
+		Statement stmnt = sc.getStatement();
+		
+		try {
+			stmnt.executeUpdate("INSERT INTO serverpeers (id, latestIP, blacklist, timestamp) " + 
+					"VALUES ("+id+", '"+latestIP+"', "+blacklist+", default)");
+		} catch (SQLException e) {
+			LOG.log(Level.INFO, e.getMessage(), e);
+		} finally {  //close all connection to database
+			sc.closeconnect();
+		}
+	}
+	
+	public void addPeerArray(String uniquefileid, String peers){  //This method writes to 'peersarray' table
+		sqlconnector sc = new sqlconnector();
+
+		Statement stmnt = sc.getStatement();
+		
+		try {
+			stmnt.executeUpdate("INSERT INTO peersarray (uniquefileid, peers) " + 
+					"VALUES ( '"+uniquefileid+"', '"+peers+"')");
+		} catch (SQLException e) {
+			LOG.log(Level.INFO, e.getMessage(), e);
+		}
+		finally {  //close all connection to database
+			sc.closeconnect();
+		}
+	}
 
 	
 	public void getBootstrapServer() {  //This method reads from 'servers' table
@@ -131,66 +194,5 @@ private static final Logger LOG = Logger.getLogger(DatabaseCalls.class.getName()
 		}	
 
 	}
-	public void addBootstrapServer(String ip, String name, int clientcount, int servercount){  //This method writes to 'servers' table
-		sqlconnector sc = new sqlconnector();
-		Statement stmnt = sc.getStatement();
-		
-		try {
-			stmnt.executeUpdate("INSERT INTO servers (ip, name, timestamp, clientcount, servercount) " + 
-					"VALUES ('"+ip+"', '"+name+"', default,"+clientcount+", "+servercount+")");
-			//"VALUES ('192.168.54.68', 'Backup01', default,2, 1)");
-			
-		} catch (SQLException e) {
-			LOG.log(Level.INFO, e.getMessage(), e);
-		}
-		finally {  //close all connection to database
-			sc.closeconnect();
-		}
-	}
 
-	public void addSwarm(String filename, int totalblocks, String peers, int peercount, int uniquefileid){   //This method writes to 'serverfile' table
-		sqlconnector sc = new sqlconnector();
-		Statement stmnt = sc.getStatement();
-				
-		try {
-			stmnt.executeUpdate("INSERT INTO serverfile (filename, totalblocks, peers, peercount, uniquefileid, filechecksum, metadatachecksum) " + 
-					"VALUES ('"+filename+"', "+totalblocks+", '"+peers+"', "+peercount+", "+uniquefileid+",'filechecksum', 'metadatachecksum')");
-			//"VALUES ('Pirates Carrabian', 10000, '192.168.2.2', 1, 2255, 'filechecksum', 'metadatachecksum' )");
-		} catch (SQLException e) {
-			LOG.log(Level.INFO, e.getMessage(), e);
-		}
-		finally {  //close all connection to database
-			sc.closeconnect();
-		}
-	}
-	
-	public void addPeers(String id, String latestIP, int blacklist){  //This method writes to 'serverpeers' table
-		sqlconnector sc = new sqlconnector();
-		Statement stmnt = sc.getStatement();
-		
-		try {
-			stmnt.executeUpdate("INSERT INTO serverpeers (id, latestIP, blacklist, timestamp) " + 
-					"VALUES ("+id+", '"+latestIP+"', "+blacklist+", default)");
-		} catch (SQLException e) {
-			LOG.log(Level.INFO, e.getMessage(), e);
-		} finally {  //close all connection to database
-			sc.closeconnect();
-		}
-	}
-	
-	public void addPeerArray(String uniquefileid, String peers){  //This method writes to 'peersarray' table
-		sqlconnector sc = new sqlconnector();
-
-		Statement stmnt = sc.getStatement();
-		
-		try {
-			stmnt.executeUpdate("INSERT INTO peersarray (uniquefileid, peers) " + 
-					"VALUES ( '"+uniquefileid+"', '"+peers+"')");
-		} catch (SQLException e) {
-			LOG.log(Level.INFO, e.getMessage(), e);
-		}
-		finally {  //close all connection to database
-			sc.closeconnect();
-		}
-	}
 }
--- src/Bootstrap/src/main/java/sql/insertsample.java
+++ src/Bootstrap/src/main/java/sql/insertsample.java
@@ -1,52 +1,53 @@
-package sql;
-
-public class insertsample {
-	
-	public void insertbdb(){
-		sqlconnector sample= new sqlconnector();
-		String writequery="";
-		sample.connector("root", "sql", "serverdb", "127.0.0.1", "3306");
-		
-		//Write Sample Data to servers table
-		writequery="insert into servers (ip,name,timestamp,clientcount,servercount) " 
-				+ " VALUES ('192.168.1.10','server-1','2015-05-11 20:00:00','10','3')";
-		sample.Update(writequery);
-		
-		//Write Sample Data to servers table
-		writequery="insert into servers (ip,name,timestamp,clientcount,servercount) " 
-				+ " VALUES ('192.168.1.20','server-2','2015-05-11 20:00:00','10','3')";
-		sample.Update(writequery);
-
-		//Write Sample Data to servers table
-		writequery="insert into servers (ip,name,timestamp,clientcount,servercount) " 
-				+ " VALUES ('192.168.1.30','server-3','2015-05-11 20:00:00','10','3')";
-		sample.Update(writequery);
-		sample.closeconnect();
-	}
-
-	public void insertcdb(){
-		sqlconnector sample= new sqlconnector();
-		String writequery="";
-		sample.connector("root", "sql", "clientdb", "127.0.0.1", "3306");
-
-		//Write Sample Data to peersarray table
-		writequery="insert into peersarray (uniquefileid,peers) " 
-				+ " VALUES ('abc123','192.168.1.200')";
-		//Writing Data to ServerDB
-		sample.Update(writequery);
-		
-		//Write Sample Data to peersarray table
-		writequery="insert into peersarray (uniquefileid,peers) " 
-				+ " VALUES ('abc123','192.168.1.201')";
-		//Writing Data to ServerDB
-		sample.Update(writequery);
-		
-		//Write Sample Data to peersarray table
-		writequery="insert into peersarray (uniquefileid,peers) " 
-				+ " VALUES ('abc123','192.168.1.202')";
-		//Writing Data to ServerDB
-		sample.Update(writequery);
-		
-		sample.closeconnect();
-	}
-}
+//
+//package sql;
+//
+//public class insertsample {
+//	
+//	public void insertbdb(){
+//		sqlconnector sample= new sqlconnector();
+//		String writequery="";
+//		sample.connector("root", "sql", "serverdb", "127.0.0.1", "3306");
+//		
+//		//Write Sample Data to servers table
+//		writequery="insert into servers (ip,name,timestamp,clientcount,servercount) " 
+//				+ " VALUES ('192.168.1.10','server-1','2015-05-11 20:00:00','10','3')";
+//		sample.Update(writequery);
+//		
+//		//Write Sample Data to servers table
+//		writequery="insert into servers (ip,name,timestamp,clientcount,servercount) " 
+//				+ " VALUES ('192.168.1.20','server-2','2015-05-11 20:00:00','10','3')";
+//		sample.Update(writequery);
+//
+//		//Write Sample Data to servers table
+//		writequery="insert into servers (ip,name,timestamp,clientcount,servercount) " 
+//				+ " VALUES ('192.168.1.30','server-3','2015-05-11 20:00:00','10','3')";
+//		sample.Update(writequery);
+//		sample.closeconnect();
+//	}
+//
+//	public void insertcdb(){
+//		sqlconnector sample= new sqlconnector();
+//		String writequery="";
+//		sample.connector("root", "sql", "clientdb", "127.0.0.1", "3306");
+//
+//		//Write Sample Data to peersarray table
+//		writequery="insert into peersarray (uniquefileid,peers) " 
+//				+ " VALUES ('abc123','192.168.1.200')";
+//		//Writing Data to ServerDB
+//		sample.Update(writequery);
+//		
+//		//Write Sample Data to peersarray table
+//		writequery="insert into peersarray (uniquefileid,peers) " 
+//				+ " VALUES ('abc123','192.168.1.201')";
+//		//Writing Data to ServerDB
+//		sample.Update(writequery);
+//		
+//		//Write Sample Data to peersarray table
+//		writequery="insert into peersarray (uniquefileid,peers) " 
+//				+ " VALUES ('abc123','192.168.1.202')";
+//		//Writing Data to ServerDB
+//		sample.Update(writequery);
+//		
+//		sample.closeconnect();
+//	}
+//}
