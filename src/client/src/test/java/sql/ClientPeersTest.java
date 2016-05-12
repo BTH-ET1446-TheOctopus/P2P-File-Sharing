562139e68b91495f59475ad725d864b620ab0c20
Iqbal, Nayyar <nayyariqbal69@yahoo.com>
2016-05-13 00:30:30 +0200

    Reading and Comparing queries of ClientPeersTest of client Db.

--- src/client/src/test/java/sql/ClientPeersTest.java
+++ src/client/src/test/java/sql/ClientPeersTest.java
@@ -24,6 +24,33 @@ public class ClientPeersTest {
   		 		String writequery="insert into clientpeers (id,latestIP,blacklist,timestamp,files,filecount)"
  				+ "VALUES ('Vf-866,'192.25.158.85',1,2016-05-12 08:19:07,'mysql-connector-5.1.39',156)";	
 		test.Update(writequery);
+		
+		/**
+ 		 * 
+ 		 * For Reading Database record from clientdb
+ 		 *
+ 		 */
+		
+		try{
+		
+ 			while(rdrs.next()){
+ 		         rddata = rdrs.getString("blacklist");	         	
+ 		      }
+		}
+		catch (Exception e) {
+	        System.out.print("Problem with the sql query: ");
+	        System.out.println(e.getMessage());
+			}
+		
+		/**
+ 		 * 
+ 		 * Assert method for comparing query written and query read.
+ 		 *
+ 		 */
+		
+ 		assertEquals(wrdata, rddata);
+ 		test.closeconnect();
+
 	}
 	
 }
