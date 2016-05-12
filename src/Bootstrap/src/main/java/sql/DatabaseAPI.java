75c7ff46616ec1b9425ecd447205c19ebcb596ba
Nyhlen, Jesper <nyhlen_91@hotmail.com>
2016-05-12 16:35:26 +0200

    Combined DBWrite and DBRead into DatabaseCalls, made interface DatabaseAPI

---
+++ src/Bootstrap/src/main/java/sql/DatabaseAPI.java
@@ -0,0 +1,14 @@
+package sql;
+
+public interface DatabaseAPI {
+
+	public void getBootstrapServer();
+	public void getSwarm();
+	public void getPeers();
+	public void getPeerArray();
+	public void addBootstrapServer(String ip, String name, int clientcount, int servercount);
+	public void addSwarm(String filename, int totalblocks, String peers, int peercount, int uniquefileid);
+	public void addPeers(String id, String latestIP, int blacklist);
+	public void addPeerArray(String uniquefileid, String peers);
+	
+}
--- src/Bootstrap/src/main/java/sql/DatabaseCalls.java
+++ src/Bootstrap/src/main/java/sql/DatabaseCalls.java
@@ -1,24 +1,20 @@
 package sql;
 
-/* class to read data from each table */
-
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
-public class DBRead {
-
-	private static final Logger LOG = Logger.getLogger(DBRead.class.getName());
+public class DatabaseCalls implements DatabaseAPI {
+	
+private static final Logger LOG = Logger.getLogger(DatabaseCalls.class.getName());
 	
 	//create an object from sqlconnector, to eb able to connect to the database
 	sqlconnector sc = new sqlconnector();
 
 	ResultSet rs = null;
 
-	public DBRead() {
-
-	}
 	
 	public void getBootstrapServer() {  //This method reads from 'servers' table
 		rs = sc.runquery("SELECT * FROM servers");
@@ -135,5 +131,66 @@ public class DBRead {
 		}	
 
 	}
+	public void addBootstrapServer(String ip, String name, int clientcount, int servercount){  //This method writes to 'servers' table
+		sqlconnector sc = new sqlconnector();
+		Statement stmnt = sc.getStatement();
+		
+		try {
+			stmnt.executeUpdate("INSERT INTO servers (ip, name, timestamp, clientcount, servercount) " + 
+					"VALUES ('"+ip+"', '"+name+"', default,"+clientcount+", "+servercount+")");
+			//"VALUES ('192.168.54.68', 'Backup01', default,2, 1)");
+			
+		} catch (SQLException e) {
+			LOG.log(Level.INFO, e.getMessage(), e);
+		}
+		finally {  //close all connection to database
+			sc.closeconnect();
+		}
+	}
 
+	public void addSwarm(String filename, int totalblocks, String peers, int peercount, int uniquefileid){   //This method writes to 'serverfile' table
+		sqlconnector sc = new sqlconnector();
+		Statement stmnt = sc.getStatement();
+				
+		try {
+			stmnt.executeUpdate("INSERT INTO serverfile (filename, totalblocks, peers, peercount, uniquefileid, filechecksum, metadatachecksum) " + 
+					"VALUES ('"+filename+"', "+totalblocks+", '"+peers+"', "+peercount+", "+uniquefileid+",'filechecksum', 'metadatachecksum')");
+			//"VALUES ('Pirates Carrabian', 10000, '192.168.2.2', 1, 2255, 'filechecksum', 'metadatachecksum' )");
+		} catch (SQLException e) {
+			LOG.log(Level.INFO, e.getMessage(), e);
+		}
+		finally {  //close all connection to database
+			sc.closeconnect();
+		}
+	}
+	
+	public void addPeers(String id, String latestIP, int blacklist){  //This method writes to 'serverpeers' table
+		sqlconnector sc = new sqlconnector();
+		Statement stmnt = sc.getStatement();
+		
+		try {
+			stmnt.executeUpdate("INSERT INTO serverpeers (id, latestIP, blacklist, timestamp) " + 
+					"VALUES ("+id+", '"+latestIP+"', "+blacklist+", default)");
+		} catch (SQLException e) {
+			LOG.log(Level.INFO, e.getMessage(), e);
+		} finally {  //close all connection to database
+			sc.closeconnect();
+		}
+	}
+	
+	public void addPeerArray(String uniquefileid, String peers){  //This method writes to 'peersarray' table
+		sqlconnector sc = new sqlconnector();
+
+		Statement stmnt = sc.getStatement();
+		
+		try {
+			stmnt.executeUpdate("INSERT INTO peersarray (uniquefileid, peers) " + 
+					"VALUES ( '"+uniquefileid+"', '"+peers+"')");
+		} catch (SQLException e) {
+			LOG.log(Level.INFO, e.getMessage(), e);
+		}
+		finally {  //close all connection to database
+			sc.closeconnect();
+		}
+	}
 }
