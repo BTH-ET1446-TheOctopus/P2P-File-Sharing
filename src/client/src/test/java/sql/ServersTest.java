5684d16929cb0dbfce564090d370e6ab31fed84e
Iqbal, Nayyar <nayyariqbal69@yahoo.com>
2016-05-12 19:56:07 +0200

    Comments and Exception Handling added to ServersTest of serverdb

--- src/client/src/test/java/sql/ServersTest.java
+++ src/client/src/test/java/sql/ServersTest.java
@@ -15,15 +15,39 @@ public class ServersTest {
  		String rddata="";
  		readquery="select name from servers where servercount >= 2 ";
   		rdrs = test.runquery(readquery);
+  		
+  		/**
+ 		 * 
+ 		 * For Writing Database record from serverdb
+ 		 *
+ 		 */
  		
  		String writequery="insert into servers (ip,name,timestamp,clientcount,servercount)"
  				+ "VALUES ('172.12.58.5','server-1',2016-05-19 08:14:07,12,3)";	
 		test.Update(writequery);
 		
+		/**
+ 		 * 
+ 		 * For Reading Database record from serverdb
+ 		 *
+ 		 */
+		
+		try{
  			while(rdrs.next()){
  		         rddata = rdrs.getString("ip");	         	
  		      }
- 	   
+		}
+		catch (Exception e) {
+        System.out.print("Problem with the sql query: ");
+        System.out.println(e.getMessage());
+		}
+		
+		/**
+ 		 * 
+ 		 * Assert method for comparing query written and query read.
+ 		 *
+ 		 */
+		
 		assertEquals(wrdata, rddata);
  		test.closeconnect();
 	}
