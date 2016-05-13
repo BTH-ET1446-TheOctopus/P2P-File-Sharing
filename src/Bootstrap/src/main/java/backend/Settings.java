165b061cd9d91470bd401cd9f4351478c830d7c5
Berntsson, Fredrik <freber@outlook.com>
2016-05-13 22:48:17 +0200

    Add blacklisted function

--- src/Bootstrap/src/main/java/backend/Settings.java
+++ src/Bootstrap/src/main/java/backend/Settings.java
@@ -1,7 +1,29 @@
 package backend;
 
+import org.glassfish.grizzly.http.server.Request;
+
 public final class Settings {
 	public static String bootstrapURL = "http://localhost:9999/";
 	public static String ntpServer = "ptbtime1.ptb.de";
 	public static int daytimeport = 13;
+	
+	/**
+	 * Function to compare incoming IP whit the ones that
+	 * are in database to see if it is blacklisted
+	 * @param request is the incoming rest request from remote client
+	 * @return true if ip was blacklisted in db
+	 */
+	public static boolean blackListedIp(Request request)
+	{
+		
+		
+		//se if ip is in db and if it is return true, if the ip 
+		//is not in db return false
+		//remove comment after fixing this
+		
+		System.out.println(request.getRemoteAddr());
+		return true;
+	}
+	
+	
 }
