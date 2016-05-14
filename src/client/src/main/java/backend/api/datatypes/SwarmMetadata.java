a0947efb8c05804d0b9072403909bb967d74b240
iiMaXii <theiimaxii@gmail.com>
2016-05-14 13:03:59 +0200

    Rewrite of API datatype SwarmMetadata from interface to class

--- src/client/src/main/java/backend/api/datatypes/SwarmMetadata.java
+++ src/client/src/main/java/backend/api/datatypes/SwarmMetadata.java
@@ -2,11 +2,45 @@ package backend.api.datatypes;
 
 import java.util.List;
 
-public interface SwarmMetadata {
-	public String getId();
-	public String getFilename();
-	public int getBlockCount();
-	public String getFileChecksum();
-	public String getMetadataChecksum();
-	public List<String> getPeers();
+public class SwarmMetadata {
+	String id;
+	String filename;
+	String blockCount;
+	String fileChecksum;
+	String metadataChecksum;
+	List<String> peers;
+
+	public SwarmMetadata(String id, String filename, String blockCount, String fileChecksum, String metadataChecksum,
+			List<String> peers) {
+		this.id = id;
+		this.filename = filename;
+		this.blockCount = blockCount;
+		this.fileChecksum = fileChecksum;
+		this.metadataChecksum = metadataChecksum;
+		this.peers = peers;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public String getBlockCount() {
+		return blockCount;
+	}
+
+	public String getFileChecksum() {
+		return fileChecksum;
+	}
+
+	public String getMetadataChecksum() {
+		return metadataChecksum;
+	}
+
+	public List<String> getPeers() {
+		return peers;
+	}
 }
