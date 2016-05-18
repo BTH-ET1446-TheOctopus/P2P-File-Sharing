13e8cadfa46a6afb7caf0feefb3bffa7ad6bf4ed
Berntsson, Fredrik <freber@outlook.com>
2016-05-18 14:22:19 +0200

    Update client database API

--- src/client/src/main/java/sql/api/DatabaseAPI.java
+++ src/client/src/main/java/sql/api/DatabaseAPI.java
@@ -6,11 +6,25 @@ import backend.api.datatypes.SwarmMetadata;
 public interface DatabaseAPI {
 
 	public void addSwarm(String id, String filename, String fileChecksum, String metadataChecksum, int blockCount);
-	public void addPeers(String swarmId, String ip);
-	public void addPeerArray(String uniquefileid, String peers);
+	public void addPeers(String swarmId, String ip); //What is the difference from the one bellow? 
+	//public void addPeerArray(String uniquefileid, String peers); //What is the difference from the one above
 	
-	public SwarmMetadata getSwarm();
+	//public SwarmMetadata getSwarm(String id);
 	public List<String> getPeers();
 	public void getPeerArray();
+	//public List<String> getBlacklist();
+	//public List<String> getBootstraps();
+	//public List<Integer> getChunks(String fileID);
+	//public boolean checkChunkAvaible(String id, Integer chunkNr);
+	//public boolean isBlacklisted(String ip);
+	
+	
+	//public void addBootstraps(ArrayList<String> ip);
+	//public void addBlacklist(ArrayList<String> ip);
+	//public void addChunks(String id, Integear chunk);
+	
+	
+	
+	
 	
 }
