adac7d20e0f21fa8f0e5f429394a59e6d8595540
mhan10 <theiimaxii@gmail.com>
2016-05-20 16:00:30 +0200

    Added command line argument for binding specific IP address

--- src/client/src/main/java/backend/Settings.java
+++ src/client/src/main/java/backend/Settings.java
@@ -1,10 +1,10 @@
 package backend;
 
 public final class Settings {
-	
-	public static final String CLIENT_PORT = "1337";
+
 	public static final String BOOTSTRAP_URL = "http://localhost:9999/rest/";
-	public static final String CLIENT_URL = "http://localhost:1337/";
+	public static final String DEFAULT_CLIENT_ADDRESS = "0.0.0.0";
+	public static final String CLIENT_PORT = "1337";
 
 	public static final int BOOTSTRAP_HELLO_INTERVAL = 2 * 60; // seconds
 	public static final int BOOTSTRAP_DATA_RETRIVAL_INTERVAL = 5 * 60; // seconds
