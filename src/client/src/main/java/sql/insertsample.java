a5828fbb97cc28c1845fc7a7eb1376a8d15b79b8
farhan ahmad <farhanahmad243@yahoo.com>
2016-05-20 13:47:00 +0200

    implement getconnpeers in clientdb api

--- src/client/src/main/java/sql/insertsample.java
+++ src/client/src/main/java/sql/insertsample.java
@@ -18,6 +18,14 @@ public class insertsample {
 //		sample.Update(writequery);
 //		writequery= "INSERT INTO clientswarm (filename, totalblocks, peers, peercount, uniquefileid, filechecksum, metadatachecksum) " 
 //				+ "VALUES ('Pirates', 10000, '192.168.2.2', 1, 'sdfsdggh22256', 'hsjsjs123', 'ghdjjd18834')";
+//		writequery= "INSERT INTO clientpeers (id, latestip, blacklist, timestamp, files, filecount) " 
+//		+ "VALUES ('abc123', 10000, '192.168.2.2', 1, 'sdfsdggh22255', 'hgjsk1345', 'hsjsj8282')";
+//
+//		sample.Update(writequery);
+//		writequery= "INSERT INTO clientpeers (id, latestip, blacklist, timestamp, files, filecount)" 
+//				+ "VALUES ('Pirates', 10000, '192.168.2.2', 1, 'sdfsdggh22256', 'hsjsjs123', 'ghdjjd18834')";
+
+		
 		
 		writequery= "INSERT INTO clientswarm (filename, totalblocks, peers, peercount, uniquefileid, filechecksum, metadatachecksum) " 
 				+ "VALUES ('pom.xml', 6, 'localhost', 1, 'abc123', 'hgjsk1345', 'hsjsj8282')";
@@ -25,23 +33,23 @@ public class insertsample {
 	
 		sample.Update(writequery);
 		
-//		//Write Sample Data to peersarray table
-//		writequery="insert into peersarray (uniquefileid,peers) " 
-//				+ " VALUES ('abc123','192.168.1.200')";
-//		//Writing Data to ServerDB
-//		sample.Update(writequery);
-//		
-//		//Write Sample Data to peersarray table
-//		writequery="insert into peersarray (uniquefileid,peers) " 
-//				+ " VALUES ('abc123','192.168.1.201')";
-//		//Writing Data to ServerDB
-//		sample.Update(writequery);
-//		
-//		//Write Sample Data to peersarray table
-//		writequery="insert into peersarray (uniquefileid,peers) " 
-//				+ " VALUES ('abc123','192.168.1.202')";
-//		//Writing Data to ServerDB
-//		sample.Update(writequery);
+		//Write Sample Data to peersarray table
+		writequery="insert into peersarray (uniquefileid,peers) " 
+				+ " VALUES ('abc123','192.168.1.200')";
+		//Writing Data to ServerDB
+		sample.Update(writequery);
+		
+		//Write Sample Data to peersarray table
+		writequery="insert into peersarray (uniquefileid,peers) " 
+				+ " VALUES ('abc123','192.168.1.201')";
+		//Writing Data to ServerDB
+		sample.Update(writequery);
+		
+		//Write Sample Data to peersarray table
+		writequery="insert into peersarray (uniquefileid,peers) " 
+				+ " VALUES ('abc123','192.168.1.202')";
+		//Writing Data to ServerDB
+		sample.Update(writequery);
 		
 		sample.closeconnect();
 	}
