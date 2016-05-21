1c77d532ca56c482cd128bbe1ab5c70a52128811
kazb15@student.bth.se <kazb15@student.bth.se>
2016-05-21 13:38:27 +0200

    Improved test case for getInactivePeers()

--- src/Bootstrap/src/test/java/sql/TestgetInactivePeers.java
+++ src/Bootstrap/src/test/java/sql/TestgetInactivePeers.java
@@ -13,14 +13,15 @@ public class TestgetInactivePeers {
 		DatabaseCalls dbc = new DatabaseCalls();
 		
 		dbc.addPeer("sd1f3dg1f5dg", "192.168.5.4", true, "2016-05-21 13:20:00");
-		dbc.addPeer("sd1f3dg1f5dh", "192.168.5.5", false, "2016-05-21 13:28:00");
+		dbc.addPeer("sd1f3dg1f5dh", "192.168.5.5", false, "2018-05-21 13:28:00");
 		
 		Peers s = dbc.getInactivePeers(180);
 		
+		assertNotNull(s.getpeers());
 		assertEquals(false , s.getpeers().isEmpty());
-		
-		
-		
+		assertEquals(1 , s.getpeers().size());
+		assertEquals("192.168.5.4" , s.getpeers().get(0));
+
 	}
 
 }
