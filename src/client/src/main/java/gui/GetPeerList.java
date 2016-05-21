dc7703012a05a2b6e351bb5282a9d856055b09f2
Iqbal, Nayyar <nayyariqbal69@yahoo.com>
2016-05-21 12:35:32 +0200

    Minor modification in GetPeerList file

--- src/client/src/main/java/gui/GetPeerList.java
+++ src/client/src/main/java/gui/GetPeerList.java
@@ -3,32 +3,19 @@ package gui;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.EventQueue;
-
-import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
-
 import backend.Backend;
-import backend.api.datatypes.SwarmMetadataShort;
 import backend.api.datatypes.*;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
-import java.awt.event.ActionEvent;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
+
 
 public class GetPeerList extends JFrame
 {
-	Backend						backend	= Backend.getInstance();
+	Backend	backend	= Backend.getInstance();
 
     public GetPeerList()
     {
@@ -51,11 +38,9 @@ public class GetPeerList extends JFrame
       //  List<String> peers = backend.getPeers();
 	
 		
-        JTable table = new JTable(data, columns);
-         
-        this.add(new JScrollPane(table));
-         
-        this.setTitle("Table Example");
+        JTable Peerstable = new JTable(data, columns);
+        this.add(new JScrollPane(Peerstable));
+        this.setTitle("PeerList");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       
         this.pack();
         this.setVisible(true);
