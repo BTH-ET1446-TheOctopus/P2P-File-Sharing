57d32791dc085f8f004bbafec011904d799d8b12
kazb15@student.bth.se <kazb15@student.bth.se>
2016-05-21 13:32:55 +0200

    Wrote test case for getInactivePeers()

---
+++ src/Bootstrap/src/test/java/sql/TestgetInactivePeers.java
@@ -0,0 +1,26 @@
+package sql;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import backend.json.Peers;
+
+public class TestgetInactivePeers {
+
+	@Test
+	public void getInactivePeersTest() {
+		
+		DatabaseCalls dbc = new DatabaseCalls();
+		
+		dbc.addPeer("sd1f3dg1f5dg", "192.168.5.4", true, "2016-05-21 13:20:00");
+		dbc.addPeer("sd1f3dg1f5dh", "192.168.5.5", false, "2016-05-21 13:28:00");
+		
+		Peers s = dbc.getInactivePeers(180);
+		
+		assertEquals(false , s.getpeers().isEmpty());
+		
+		
+		
+	}
+
+}
