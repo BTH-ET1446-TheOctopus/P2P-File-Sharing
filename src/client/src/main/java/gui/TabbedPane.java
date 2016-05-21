0b31178e9964ad25bed7bc1520b2edb8c11990da
Kukkapalli,NagaVyshnavi <vyshnavi994@gmail.com>
2016-05-22 01:43:25 +0200

    New Buttons Added in TabbedPane

--- src/client/src/main/java/gui/TabbedPane.java
+++ src/client/src/main/java/gui/TabbedPane.java
@@ -1,6 +1,4 @@
 package gui;
-
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -16,23 +14,26 @@ public class TabbedPane extends JFrame {
     	setup();
     
     }
-       
-    	private void setup(){
-        setTitle("Settings Panel");
+       	private void setup(){
+        setTitle("Swarm Panel");
         JTabbedPane jmenu = new JTabbedPane();
 		jmenu.setBounds(0, 0, 600, 380);
-
         getContentPane().add(jmenu);
-        setSize(400,400);
+        setSize(400,400);        
      
         JPanel tab1 = new JPanel();
         jmenu.addTab("General", tab1);
-        
         JCheckBox cb = new JCheckBox("Dark Peer", true);
         tab1.add(cb);
-        JButton btn1 = new JButton("Apply Changes");
+        JButton btn1 = new JButton("Add peer");
         tab1.add(btn1);
-         
+        JButton btn2 = new JButton("Refresh");
+        tab1.add(btn2);
+        JButton btn3 = new JButton("Cancel");
+        tab1.add(btn3);
+        JButton btn4 = new JButton("Apply Changes");
+        tab1.add(btn4);
+        
         JPanel tab2 = new JPanel();      
         JLabel lb2 = new JLabel();
         lb2.setText("More Features");
